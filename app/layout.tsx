a7b34f154735f8602bf04733e0948f7957567a32
nkfab <fabjeezy@gmail.com>
2025-03-17 15:43:36 +0200

    add suspense page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/custom/theme-provider";
 import { Navbar } from "@/components/custom/navbar";
 import { AuthProvider } from "@/components/custom/auth-provider";
 import "@/app/globals.css";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,13 +29,15 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <AuthProvider>
-            <div className="min-h-screen flex flex-col">
-              <Navbar />
-              <main className="flex-1">{children}</main>
-              <Toaster />
-            </div>
-          </AuthProvider>
+          <Suspense>
+            <AuthProvider>
+              <div className="min-h-screen flex flex-col">
+                <Navbar />
+                <main className="flex-1">{children}</main>
+                <Toaster />
+              </div>
+            </AuthProvider>
+          </Suspense>
         </ThemeProvider>
       </body>
     </html>
